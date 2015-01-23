49d2248d3b70b26d565e5d74dd7d1b39dc807573
dongjun <250191950@qq.com>
2015-01-23 16:53:11 +0800

    action默认为多实例 Signed-off-by: dongjun <250191950@qq.com>

--- common/com/common/action/BaseAction.java
+++ common/com/common/action/BaseAction.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.struts2.interceptor.ApplicationAware;
 import org.apache.struts2.interceptor.RequestAware;
 import org.apache.struts2.interceptor.SessionAware;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -18,6 +19,7 @@ import com.opensymphony.xwork2.ActionSupport;
  * 
  */
 @Controller
+@Scope("prototype")
 public class BaseAction extends ActionSupport implements SessionAware,
 		RequestAware, ApplicationAware {
 	/**
