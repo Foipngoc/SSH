dd8df2437337dbd6c3ea7885882a27c5dab25164
dongjun <250191950@qq.com>
2015-04-06 21:13:02 +0800

    框架【文件md5】 修改获取文件md5的方法
    原mappedbytebuffer会导致文件被占用直到jvm自己释放该文件句柄，
    从而导致文件不能被释放。
    
    Signed-off-by: dongjun <250191950@qq.com>

--- common/com/common/utils/FileMd5.java
+++ common/com/common/utils/FileMd5.java
@@ -2,11 +2,7 @@ package com.common.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 
 /**
@@ -18,27 +14,23 @@ import java.security.MessageDigest;
 public class FileMd5 {
 	public static String getMd5ByFile(File file) {
 		String value = null;
-		FileInputStream in;
+		FileInputStream in = null;
 		try {
 			in = new FileInputStream(file);
-		} catch (FileNotFoundException e1) {
-			return null;
-		}
-		try {
-			MappedByteBuffer byteBuffer = in.getChannel().map(
-					FileChannel.MapMode.READ_ONLY, 0, file.length());
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			md5.update(byteBuffer);
+            int length;
+            byte[] buffer = new byte[10240];
+            while ((length = in.read(buffer)) != -1) {
+            	md5.update(buffer, 0, length);
+            }
 			BigInteger bi = new BigInteger(1, md5.digest());
 			value = bi.toString(16);
 		} catch (Exception e) {
-			e.printStackTrace();
+			return null;
 		} finally {
-			if (null != in) {
-				try {
-					in.close();
-				} catch (IOException e) {
-				}
+			try {
+				in.close();
+			} catch (Exception e) {
 			}
 		}
 		return value;
