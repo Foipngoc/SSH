252bac4ee6ca24eecb276e66df5bb2113e3e2681
dongjun <250191950@qq.com>
2015-04-29 16:29:40 +0800

    解析更新模块文件夹大小写问题
    Signed-off-by: dongjun <250191950@qq.com>

--- module/appVersionCheck/src/com/module/appVersionCheck/dao/AppInfoDao.java
+++
@@ -1,8 +0,0 @@
-package com.module.appversioncheck.dao;
-
-import com.common.dao.BaseDao;
-import com.module.appversioncheck.model.AppInfo;
-
-public interface AppInfoDao extends BaseDao<AppInfo> {
-
-}
