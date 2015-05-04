e4e3dc9ce93b50e0261b2390f04bfdc6290ee3be
dongjun <250191950@qq.com>
2015-05-04 14:01:57 +0800

    CRON表达式如何生成
    Signed-off-by: dongjun <250191950@qq.com>

--- example/com/timer/Timer.java
+++ example/com/timer/Timer.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class Timer {
 	private int index = 0;
 	
+	//@Scheduled(cron) cron表达式可用网址http://www.bejson.com/cronCreator/生成
 	@Scheduled(fixedRate=5000)
 	public void doTimer(){
 		System.out.println("Timer: "+index);
