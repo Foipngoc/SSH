199335e656b59bfe27eb5ac593ce8a5fb0905b5e
dongjun <250191950@qq.com>
2015-05-11 13:34:57 +0800

    完善DateTimeUtil工具
    Signed-off-by: dongjun <250191950@qq.com>

--- common/com/common/utils/DateTimeUtil.java
+++ common/com/common/utils/DateTimeUtil.java
@@ -2,6 +2,7 @@ package com.common.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -58,10 +59,15 @@ public class DateTimeUtil {
 	 * 
 	 * @throws ParseException
 	 */
-	public static Date getDateByStringFmt(String timeString, String fmt)
-			throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat(fmt);
-		Date date = sdf.parse(timeString);
+	public static Date getDateByStringFmt(String timeString, String fmt) {
+		Date date = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat(fmt);
+			date = sdf.parse(timeString);
+		} catch (Exception e) {
+			return null;
+		}
+
 		return date;
 	}
 
@@ -70,10 +76,8 @@ public class DateTimeUtil {
 	 * 
 	 * @throws ParseException
 	 */
-	public static Date getDateByStringFmt(String timeString) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date date = sdf.parse(timeString);
-		return date;
+	public static Date getDateByStringFmt(String timeString) {
+		return getDateByStringFmt(timeString, "yyyy-MM-dd HH:mm:ss");
 	}
 
 	/**
@@ -84,4 +88,292 @@ public class DateTimeUtil {
 	public static Date getCurrDate() {
 		return new Date();
 	}
+
+	/**
+	 * 取得日期：年
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static int getYear(Date date) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		int year = c.get(Calendar.YEAR);
+		return year;
+	}
+
+	/**
+	 * 取得日期：月
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static int getMonth(Date date) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		int month = c.get(Calendar.MONTH);
+		return month + 1;
+	}
+	
+	/** 
+     * 取得日期：日
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getDay(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        int da = c.get(Calendar.DAY_OF_MONTH);  
+        return da;  
+    } 
+    
+    /** 
+     * 取得当天日期是周几 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getWeekDay(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        int week_of_year = c.get(Calendar.DAY_OF_WEEK);  
+        return week_of_year - 1;  
+    } 
+    
+    /** 
+     * 取得一年的第几周 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getWeekOfYear(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        int week_of_year = c.get(Calendar.WEEK_OF_YEAR);  
+        return week_of_year;  
+    }  
+    
+    /** 
+     * 取得月的剩余天数 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getRemainDayOfMonth(Date date) {  
+        int dayOfMonth = getDayOfMonth(date);  
+        int day = getPassDayOfMonth(date);  
+        return dayOfMonth - day;  
+    }  
+    
+    /** 
+     * 取得月已经过的天数 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getPassDayOfMonth(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        return c.get(Calendar.DAY_OF_MONTH);  
+    }  
+  
+    /** 
+     * 取得月天数 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getDayOfMonth(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        return c.getActualMaximum(Calendar.DAY_OF_MONTH);  
+    }  
+  
+    /** 
+     * 取得月第一天 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static Date getFirstDateOfMonth(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        c.set(Calendar.DAY_OF_MONTH, c.getActualMinimum(Calendar.DAY_OF_MONTH));  
+        return c.getTime();  
+    }  
+  
+    /** 
+     * 取得月最后一天 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static Date getLastDateOfMonth(Date date) {  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        c.set(Calendar.DAY_OF_MONTH, c.getActualMaximum(Calendar.DAY_OF_MONTH));  
+        return c.getTime();  
+    }  
+  
+    /** 
+     * 取得季度第一天 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static Date getFirstDateOfQuarter(Date date) {  
+        return getFirstDateOfMonth(getQuarterDate(date)[0]);  
+    }  
+  
+    /** 
+     * 取得季度最后一天 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static Date getLastDateOfQuarter(Date date) {  
+        return getLastDateOfMonth(getQuarterDate(date)[2]);  
+    }  
+  
+    /** 
+     * 取得季度天数 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getDayOfQuarter(Date date) {  
+        int day = 0;  
+        Date[] seasonDates = getQuarterDate(date);  
+        for (Date date2 : seasonDates) {  
+            day += getDayOfMonth(date2);  
+        }  
+        return day;  
+    }  
+  
+    /** 
+     * 取得季度剩余天数 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getRemainDayOfQuareter(Date date) {  
+        return getDayOfQuarter(date) - getPassDayOfQuarter(date);  
+    }  
+  
+    /** 
+     * 取得季度已过天数 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getPassDayOfQuarter(Date date) {  
+        int day = 0;  
+  
+        Date[] quarterDates = getQuarterDate(date);  
+  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        int month = c.get(Calendar.MONTH);  
+  
+        if (month == Calendar.JANUARY || month == Calendar.APRIL  
+                || month == Calendar.JULY || month == Calendar.OCTOBER) {// 季度第一个月  
+            day = getPassDayOfMonth(quarterDates[0]);  
+        } else if (month == Calendar.FEBRUARY || month == Calendar.MAY  
+                || month == Calendar.AUGUST || month == Calendar.NOVEMBER) {// 季度第二个月  
+            day = getDayOfMonth(quarterDates[0])  
+                    + getPassDayOfMonth(quarterDates[1]);  
+        } else if (month == Calendar.MARCH || month == Calendar.JUNE  
+                || month == Calendar.SEPTEMBER || month == Calendar.DECEMBER) {// 季度第三个月  
+            day = getDayOfMonth(quarterDates[0]) + getDayOfMonth(quarterDates[1])  
+                    + getPassDayOfMonth(quarterDates[2]);  
+        }  
+        return day;  
+    }  
+  
+    /** 
+     * 取得季度月 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static Date[] getQuarterDate(Date date) {  
+        Date[] season = new Date[3];  
+  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+  
+        int nQuarter = getQuarter(date);  
+        if (nQuarter == 1) {// 第一季度  
+            c.set(Calendar.MONTH, Calendar.JANUARY);  
+            season[0] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.FEBRUARY);  
+            season[1] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.MARCH);  
+            season[2] = c.getTime();  
+        } else if (nQuarter == 2) {// 第二季度  
+            c.set(Calendar.MONTH, Calendar.APRIL);  
+            season[0] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.MAY);  
+            season[1] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.JUNE);  
+            season[2] = c.getTime();  
+        } else if (nQuarter == 3) {// 第三季度  
+            c.set(Calendar.MONTH, Calendar.JULY);  
+            season[0] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.AUGUST);  
+            season[1] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.SEPTEMBER);  
+            season[2] = c.getTime();  
+        } else if (nQuarter == 4) {// 第四季度  
+            c.set(Calendar.MONTH, Calendar.OCTOBER);  
+            season[0] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.NOVEMBER);  
+            season[1] = c.getTime();  
+            c.set(Calendar.MONTH, Calendar.DECEMBER);  
+            season[2] = c.getTime();  
+        }  
+        return season;  
+    }  
+  
+    /** 
+     *  
+     * 1 第一季度 2 第二季度 3 第三季度 4 第四季度 
+     *  
+     * @param date 
+     * @return 
+     */  
+    public static int getQuarter(Date date) {  
+  
+        int quarter = 0;  
+  
+        Calendar c = Calendar.getInstance();  
+        c.setTime(date);  
+        int month = c.get(Calendar.MONTH);  
+        switch (month) {  
+        case Calendar.JANUARY:  
+        case Calendar.FEBRUARY:  
+        case Calendar.MARCH:  
+        	quarter = 1;  
+            break;  
+        case Calendar.APRIL:  
+        case Calendar.MAY:  
+        case Calendar.JUNE:  
+        	quarter = 2;  
+            break;  
+        case Calendar.JULY:  
+        case Calendar.AUGUST:  
+        case Calendar.SEPTEMBER:  
+        	quarter = 3;  
+            break;  
+        case Calendar.OCTOBER:  
+        case Calendar.NOVEMBER:  
+        case Calendar.DECEMBER:  
+        	quarter = 4;  
+            break;  
+        default:  
+            break;  
+        }  
+        return quarter;  
+    }  
 }
