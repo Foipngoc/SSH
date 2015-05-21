8c57aae2cf2eb4998aadda777d056af603bb4626
dongjun <250191950@qq.com>
2015-05-21 19:49:39 +0800

    修改Transactional导入文件
    Signed-off-by: dongjun <250191950@qq.com>

--- common/com/common/service/BaseService.java
+++ common/com/common/service/BaseService.java
@@ -1,10 +1,9 @@
 package com.common.service;
 
-import javax.transaction.Transactional;
-
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.common.framework.CXFFilter;
 
