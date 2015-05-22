53fcc3c76db9d2092134498dcd37830b3189c067
dongjun <250191950@qq.com>
2015-05-22 10:17:02 +0800

    BaseResultFailed支持自定义错误结果码， 注不能定义结果码为0
    Signed-off-by: dongjun <250191950@qq.com>

--- common/com/common/action/result/BaseResultFailed.java
+++ common/com/common/action/result/BaseResultFailed.java
@@ -5,6 +5,10 @@ import java.util.Map;
 /**
  * 失败结果
  * 
+ * 默认resultcode为-1
+ * 
+ * 也可以自行指定resultcode，但是不能为0
+ * 
  * @author DJ
  * 
  */
@@ -23,5 +27,30 @@ public class BaseResultFailed extends BaseResult {
 
 	public BaseResultFailed(String reason, Object obj) {
 		super(RESULT_FAILED, reason, obj);
+	}
+
+	public BaseResultFailed(int resultcode) {
+		super(resultcode, "未知原因");
+		if (resultcode == RESULT_OK)
+			throw new RuntimeException("失败的结果码不能为0");
+	}
+
+	public BaseResultFailed(int resultcode, String reason) {
+		super(resultcode, reason);
+		if (resultcode == RESULT_OK)
+			throw new RuntimeException("失败的结果码不能为0");
+	}
+
+	public BaseResultFailed(int resultcode, String reason,
+			Map<String, Object> map) {
+		super(resultcode, reason, map);
+		if (resultcode == RESULT_OK)
+			throw new RuntimeException("失败的结果码不能为0");
+	}
+
+	public BaseResultFailed(int resultcode, String reason, Object obj) {
+		super(resultcode, reason, obj);
+		if (resultcode == RESULT_OK)
+			throw new RuntimeException("失败的结果码不能为0");
 	}
 }
